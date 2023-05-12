4638da9bd1c801b866c90adaf7a6569b68662992
abrleva8 <abrleva8@gmail.com>
2023-05-12 17:07:13 +0300

    add

---
+++ Channel.cs
@@ -0,0 +1,13 @@
+﻿namespace Don_tKnowHowToNameThis; 
+
+public class Channel {
+    public readonly double W; //ok 
+    public readonly double H; //ok
+    public readonly double L; //ok
+
+    public Channel(double w, double h, double l) {
+        W = w;
+        H = h;
+        L = l;
+    }
+}
