9b2b3085bfe0d851f14e836f0fb1b82b9d79f22d
abrleva8 <abrleva8@gmail.com>
2023-05-13 20:01:18 +0300

    add login window

---
+++ LoginException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using K4os.Compression.LZ4.Streams.Frames;
+
+namespace Don_tKnowHowToNameThis; 
+
+public class LoginException : Exception {}
+
+public class NotSuchUserException : LoginException {
+    private readonly string _login;
+
+    public NotSuchUserException(string login) {
+        _login = login;
+    }
+
+    public override string ToString() {
+        return $"Пользователь {_login} не найден";
+    }
+}
+
+public class BadPasswordException : LoginException {
+    private readonly string _login;
+
+    public BadPasswordException(string login) {
+        _login = login;
+    }
+
+    public override string ToString() {
+        return $"Пароль для {_login} неверный";
+    }
+} 
---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿namespace Don_tKnowHowToNameThis; 
+
+using System.IO;
+using System.Windows;
+
+
+/// <summary>
+///     Логика взаимодействия для LoginWindow.xaml
+/// </summary>
+public partial class LoginWindow {
+    public LoginWindow() {
+        InitializeComponent();
+    }
+
+    private void StartUserWindow(object sender, RoutedEventArgs e) {
+        var currentLogin = LoginTextBox.Text;
+        var currentPassword = PasswordTextBox.Password;
+        var user = "";
+        try {
+            user = Check(currentPassword, currentLogin);
+        } catch (LoginException loginException) {
+            MessageBox.Show(loginException.ToString());
+        }
+
+        switch (user) {
+            case "admin":
+                break;
+            case "user":
+                new MainWindow().Show();
+                break;
+        }
+        
+    }
+
+    private static string Check(string currentPassword, string currentLogin) {
+        const string textFile = "C:\\Users\\abrle\\RiderProjects\\Chemical\\passwords.txt";
+        var lines = File.ReadAllLines(textFile);
+
+        foreach (var line in lines[1..]) {
+            var data = line.Split(',');
+            var login = data[0];
+            var password = data[1];
+
+            if (login != currentLogin) continue;
+            if (password == currentPassword) {
+                return login;
+            }
+
+            throw new BadPasswordException(login);
+        }
+
+        throw new NotSuchUserException(currentLogin);
+    }
+}
